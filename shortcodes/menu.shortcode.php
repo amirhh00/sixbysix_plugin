c3bbb89ca9fb9a6bcc20308a791230e4a42b4811
amirhh00 <amirhh00@gmail.com>
2024-07-21 13:47:35 +0100

    menu style fix

--- shortcodes/menu.shortcode.php
+++ shortcodes/menu.shortcode.php
@@ -9,15 +9,20 @@ function menus_shortcode($atts, $content = null)
   $class = $attributes['class'] ? ' class="' . esc_attr($attributes['class']) . '"' : '';
   $id = $attributes['id'] ? ' id="' . esc_attr($attributes['id']) . '"' : '';
   $menu_items = get_option('restaurant_menu_items', array());
-  $output = '<div' . $id . $class . '>';
+  $output = '';
+  $output .= '<div id="sixbysixMenus">';
+  $output .= '<ul' . $id . $class . '>';
   foreach ($menu_items as $item) {
-    $output .= '<div class="menu-item">';
+    $output .= '<li' . (!empty($item['link']) && $item['link'] !== '#' ? ' onclick="window.open(\'' . esc_url($item['link']) . '\', \'_blank\')"' : '') . ' class="menu-item">';
     $output .= '<h3>' . esc_html($item['name']) . '</h3>';
-    $output .= '<p><strong>Days Available:</strong> ' . esc_html($item['days']) . '</p>';
-    $output .= '<p><strong>Time Available:</strong> ' . esc_html($item['time']) . '</p>';
-    $output .= '<p><a href="' . esc_url($item['link']) . '" target="_blank">More Info</a></p>';
+    $output .= '<div>';
+    $output .= '<p>' . esc_html($item['days']) . '</p>';
+    $output .= '<p>' . esc_html($item['time']) . '</p>';
     $output .= '</div>';
+    $output .= '<a href="' . esc_url($item['link']) . '"' . (!empty($item['link']) && $item['link'] !== '#' ? ' target="_blank"' : '') . '>View Menu</a>';
+    $output .= '</li>';
   }
+  $output .= '</ul>';
   $output .= '</div>';
   return $output;
 }
@@ -29,3 +34,5 @@ function enqueue_shortcode_styles()
 {
   wp_enqueue_style('shortcode-styles', plugin_dir_url(__FILE__) . './styles/menu.css');
 }
+
+add_action('wp_enqueue_scripts', 'enqueue_shortcode_styles');
