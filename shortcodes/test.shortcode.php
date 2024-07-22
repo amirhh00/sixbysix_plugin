4f7f63f0911b67fcba33740267ca0888894c0d25
amirhh00 <amirhh00@gmail.com>
2024-07-22 15:20:59 +0100

    chore: Update version to 1.1.9 and change button shortcode to div shortcode

--- shortcodes/test.shortcode.php
+++ shortcodes/test.shortcode.php
@@ -1,6 +1,6 @@
 <?php
 
-function custom_button_shortcode($atts, $content = null)
+function custom_shortcode($atts, $content = null)
 {
   $attributes = shortcode_atts(array(
     'class' => '',
@@ -10,6 +10,6 @@ function custom_button_shortcode($atts, $content = null)
   $class = $attributes['class'] ? ' class="' . esc_attr($attributes['class']) . '"' : '';
   $id = $attributes['id'] ? ' id="' . esc_attr($attributes['id']) . '"' : '';
 
-  return '<button' . $class . $id . '>' . do_shortcode($content) . "test" . '</button>';
+  return '<div' . $class . $id . '>' . do_shortcode($content) . "test" . '</div>';
 }
-add_shortcode('button', 'custom_button_shortcode');
+add_shortcode('div', 'custom_shortcode');
