c6fee35163a3b66d4f515cbdf48e48f63b82af30
amirhh00 <amirhh00@gmail.com>
2024-07-22 15:27:24 +0100

    Refactor shortcode to use opening and closing tags for div

--- shortcodes/test.shortcode.php
+++ shortcodes/test.shortcode.php
@@ -10,6 +10,12 @@ function custom_shortcode($atts, $content = null)
   $class = $attributes['class'] ? ' class="' . esc_attr($attributes['class']) . '"' : '';
   $id = $attributes['id'] ? ' id="' . esc_attr($attributes['id']) . '"' : '';
 
-  return '<div' . $class . $id . '>' . do_shortcode($content) . "test" . '</div>';
+  return '<div' . $class . $id . '>' . do_shortcode($content);
 }
 add_shortcode('div', 'custom_shortcode');
+
+function custom_shortcode_end($atts, $content = null)
+{
+  return '</div>';
+}
+add_shortcode('/div', 'custom_shortcode_end');
