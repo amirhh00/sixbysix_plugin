ba97aa4aeaa8025bd2a3d14890afbbe08090353a
amirhh00 <amirhh00@gmail.com>
2024-08-15 00:57:00 +0100

    chore: Bump version to 1.1.41 and update plugin information

--- index.php
+++ index.php
@@ -1,18 +1,30 @@
 <?php
-/*
-Plugin Name: SixOneSix
-Description: Registers custom shortcodes and helper functions.
-Version: 1.1.40
-Author: Amirhossein
-Plugin URI: https://github.com/amirhh00/sixbysix_plugin
-Author URI: https://github.com/amirhh00
-GitHub Plugin URI: https://github.com/amirhh00/sixbysix_plugin
-GitHub Branch: main
-*/
+
+/**
+ * Plugin Name:       SixOneSix
+ * Description:       Registers custom shortcodes and helper functions.
+ * Requires at least: 6.1
+ * Requires PHP:      7.0
+ * Version:           1.1.41
+ * Author:            Amirhossein
+ * License:           GPL-2.0-or-later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       copyright-date-block
+ * Plugin URI:        https://github.com/amirhh00/sixbysix_plugin
+ * Author URI:        https://github.com/amirhh00
+ * GitHub Plugin URI: https://github.com/amirhh00/sixbysix_plugin
+ * GitHub Branch:     main
+ * 
+ * @package CreateBlock
+ */
 
 require_once plugin_dir_path(__FILE__) . 'shortcodes/index.php';
 require_once plugin_dir_path(__FILE__) . 'dashboard/index.php';
 require_once plugin_dir_path(__FILE__) . 'injections/index.php';
+
+if (! defined('ABSPATH')) {
+  exit; // Exit if accessed directly.
+}
 
 function get_active_plugin_version()
 {
