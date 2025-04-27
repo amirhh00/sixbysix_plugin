b37ba0f6477dbd5cf83676b54ba62f022ca6869d
amirhh00 <amirhh00@gmail.com>
2025-04-27 13:59:33 +0100

    feat: bump version to 1.3.5, update menu shortcode to include inline CSS and remove external stylesheet

--- shortcodes/menu.shortcode.php
+++ shortcodes/menu.shortcode.php
@@ -6,61 +6,150 @@ function menus_shortcode($atts, $content = null)
     'class' => '',
     'id' => '',
   ), $atts);
-  // load css
-  wp_enqueue_style('restaurant-menu', plugin_dir_url(__FILE__) . './styles/menu.css');
+
   $class = $attributes['class'] ? ' class="' . esc_attr($attributes['class']) . '"' : '';
   $id = $attributes['id'] ? ' id="' . esc_attr($attributes['id']) . '"' : '';
+
+  // Query menu items
   $args = array(
     'post_type' => 'menu_item',
-    'posts_per_page' => -1, // Retrieve all menu items
-    'orderby' => 'menu_order', // Order by the custom order set in the admin
-    'order' => 'ASC' // Ascending order
+    'posts_per_page' => -1,
+    'orderby' => 'menu_order',
+    'order' => 'ASC'
   );
 
-  ob_start();
   $query = new WP_Query($args);
+  $menu_items = [];
+
   if ($query->have_posts()) {
-    $menu_items = [];
     while ($query->have_posts()) {
       $query->the_post();
       $post_id = get_the_ID();
-      $menu_name = get_the_title();
-      $menu_days = get_post_meta($post_id, 'days_available', true);
-      $menu_time = get_post_meta($post_id, 'time_available', true);
-      $menu_link = get_post_meta($post_id, 'link', true);
-      $button_text = get_post_meta($post_id, 'button_text', true);
-      $menu_item = [
-        'name' => $menu_name,
-        'days' => $menu_days,
-        'time' => $menu_time,
-        'link' => $menu_link,
-        'button_text' => $button_text ? $button_text : 'View Menu'
+      $menu_items[] = [
+        'name' => get_the_title(),
+        'days' => get_post_meta($post_id, 'days_available', true),
+        'time' => get_post_meta($post_id, 'time_available', true),
+        'link' => get_post_meta($post_id, 'link', true),
+        'button_text' => get_post_meta($post_id, 'button_text', true) ?: 'View Menu'
       ];
-      $menu_items[] = $menu_item;
     }
     wp_reset_postdata();
   }
 
-  $output = '';
+  // Return early if no menu items
   if (empty($menu_items)) {
-    return ob_get_clean() . $output;
+    return '';
   }
 
-  $output .= '<div id="sixbysixMenus">';
-  $output .= '<ul' . $id . $class . '>';
-  foreach ($menu_items as $item) {
-    $output .= '<li' . (!empty($item['link']) && $item['link'] !== '#' ? ' onclick="window.open(\'' . esc_url($item['link']) . '\', \'_blank\')"' : '') . ' class="menu-item">';
-    $output .= '<h3>' . esc_html($item['name']) . '</h3>';
-    $output .= '<div>';
-    $output .= '<p>' . esc_html($item['days']) . '</p>';
-    $output .= '<p>' . esc_html($item['time']) . '</p>';
-    $output .= '</div>';
-    $output .= '<a href="' . esc_url($item['link']) . '"' . (!empty($item['link']) && $item['link'] !== '#' ? ' target="_blank"' : '') . '>' . esc_html($item['button_text']) . '</a>';
-    $output .= '</li>';
-  }
-  $output .= '</ul>';
-  $output .= '</div>';
-  return ob_get_clean() . $output;
+  // Include inline CSS
+  $css = <<<CSS
+<style>
+.u-content-background {
+    background-color: black;
 }
+
+.u-content-background .entry-title {
+    text-align: center;
+    color: var(--ast-global-color-5, white);
+}
+
+#sixbysixMenus {
+    padding: 2rem 1rem 2rem 1rem;
+    background-color: inherit;
+    margin: 0 auto;
+}
+
+#sixbysixMenus * {
+    margin: 0;
+    margin-bottom: 0 !important;
+    padding: 0;
+    box-sizing: border-box;
+    outline: none;
+    color: var(--ast-global-color-5, white);
+    transition: all 0.2s ease-in;
+}
+
+#sixbysixMenus ul {
+    display: grid;
+    grid-template-columns: repeat(3, minmax(250px, 1fr));
+    gap: 1rem;
+    list-style: none;
+}
+
+@media screen and (max-width: 860px) {
+    #sixbysixMenus ul {
+        grid-template-columns: 1fr;
+        place-items: center;
+    }
+
+    #sixbysixMenus ul .menu-item {
+        width: 100%;
+    }
+}
+
+#sixbysixMenus ul .menu-item {
+    display: flex;
+    max-width: 350px;
+    flex-direction: column;
+    justify-content: space-between;
+    border: 2px solid var(--ast-global-color-0, #c2b276);
+    padding: 0.5rem 1rem;
+    gap: 0.5rem;
+    text-align: center;
+    background-color: var(--ast-global-color-0, #c2b276);
+}
+
+#sixbysixMenus ul .menu-item div {
+    font-size: 10px;
+}
+
+#sixbysixMenus ul .menu-item h3,
+#sixbysixMenus ul .menu-item a {
+    font-size: 1rem;
+    font-weight: 700;
+    white-space: nowrap;
+    text-decoration: none;
+    margin: 0 auto;
+    border-bottom: 2px solid transparent;
+}
+
+#sixbysixMenus ul li.menu-item:hover {
+    background-color: var(--ast-global-color-6, black);
+    cursor: pointer;
+}
+</style>
+CSS;
+
+  // Build HTML output using heredoc syntax
+  $html = <<<HTML
+<div id="sixbysixMenus">
+    <ul{$id}{$class}>
+HTML;
+
+  foreach ($menu_items as $item) {
+    $onclick = (!empty($item['link']) && $item['link'] !== '#') ?
+      ' onclick="window.open(\'' . esc_url($item['link']) . '\', \'_blank\')"' : '';
+    $target = (!empty($item['link']) && $item['link'] !== '#') ? ' target="_blank"' : '';
+
+    $html .= <<<HTML
+        <li class="menu-item"{$onclick}>
+            <h3>{$item['name']}</h3>
+            <div>
+                <p>{$item['days']}</p>
+                <p>{$item['time']}</p>
+            </div>
+            <a href="{$item['link']}"{$target}>{$item['button_text']}</a>
+        </li>
+HTML;
+  }
+
+  $html .= <<<HTML
+    </ul>
+</div>
+HTML;
+
+  return $css . $html;
+}
+
 
 add_shortcode('rmenu', 'menus_shortcode');
