e74331f6a0c8cb99f56a57833ff337b3c0497cf2
amirhh00 <amirhh00@gmail.com>
2025-05-23 05:17:57 +0100

    feat: update version to 1.3.27 and enhance calendar page layout

--- dashboard/calendar.php
+++ dashboard/calendar.php
@@ -83,32 +83,33 @@ function sixonesix_calendar_page()
     <?php endif; ?>
     <div style="display: flex; gap: 16px; justify-content: space-between; width: 100%;">
       <button class="btn btn-reverted" id="prevMonth">Previous</button>
-      <h3 class="currentMonth">
-        <?php
-        $monthNames = [
-          'January',
-          'February',
-          'March',
-          'April',
-          'May',
-          'June',
-          'July',
-          'August',
-          'September',
-          'October',
-          'November',
-          'December'
-        ];
-        echo $monthNames[date('n') - 1] . ' ' . date('Y');
-        ?>
-      </h3>
+      <div class="flex flex-col w-min mx-auto justify-center items-center">
+        <h3 class="currentMonth !whitespace-nowrap !mb-0">
+          <?php
+          $monthNames = [
+            'January',
+            'February',
+            'March',
+            'April',
+            'May',
+            'June',
+            'July',
+            'August',
+            'September',
+            'October',
+            'November',
+            'December'
+          ];
+          echo $monthNames[date('n') - 1] . ' ' . date('Y');
+          ?>
+        </h3>
+        <button class="btn hover:*:fill-white !p-1 !min-w-0 aspect-square" style="border: none;" id="reloadMonth">
+          <svg xmlns="http://www.w3.org/2000/svg" style="width: 30px; height:30px;" class="fill-black" viewBox="0 0 512 512">
+            <path d="M142.9 142.9c-17.5 17.5-30.1 38-37.8 59.8c-5.9 16.7-24.2 25.4-40.8 19.5s-25.4-24.2-19.5-40.8C55.6 150.7 73.2 122 97.6 97.6c87.2-87.2 228.3-87.5 315.8-1L455 55c6.9-6.9 17.2-8.9 26.2-5.2s14.8 12.5 14.8 22.2l0 128c0 13.3-10.7 24-24 24l-8.4 0c0 0 0 0 0 0L344 224c-9.7 0-18.5-5.8-22.2-14.8s-1.7-19.3 5.2-26.2l41.1-41.1c-62.6-61.5-163.1-61.2-225.3 1zM16 312c0-13.3 10.7-24 24-24l7.6 0 .7 0L168 288c9.7 0 18.5 5.8 22.2 14.8s1.7 19.3-5.2 26.2l-41.1 41.1c62.6 61.5 163.1 61.2 225.3-1c17.5-17.5 30.1-38 37.8-59.8c5.9-16.7 24.2-25.4 40.8-19.5s25.4 24.2 19.5 40.8c-10.8 30.6-28.4 59.3-52.9 83.8c-87.2 87.2-228.3 87.5-315.8 1L57 457c-6.9 6.9-17.2 8.9-26.2 5.2S16 449.7 16 440l0-119.6 0-.7 0-7.6z" />
+          </svg>
+        </button>
+      </div>
       <button class="btn btn-reverted" id="nextMonth">Next</button>
-    </div>
-    <div style="display: flex; justify-content: center;">
-      <!-- go to current month button with js -->
-      <button class="btn" style="border: none;" id="reloadMonth">
-        &#10227;
-      </button>
     </div>
     <br>
     <div id="daysinweek" style="display: grid; grid-template-columns: repeat(7, 1fr); gap: 8px; max-width: 100%; overflow-x: auto;">
@@ -397,7 +398,7 @@ function sixonesix_calendar_page()
                 }
               }
               // if the background image is set, change elements
-              calendar.style.backgroundImage = allEventsForMonth.monthBg ? `url(${allEventsForMonth.monthBg})` : '';
+              calendar.style.backgroundImage = allEventsForMonth.monthBg ? `url(${allEventsForMonth.monthBg})` : 'url(https://placehold.co/600x400@3x/000000/000?text=img)';
               // set the background image to the selected image
               <?php if ($is_admin_dashboardPage) : ?>
                 if (allEventsForMonth.monthBg) {
